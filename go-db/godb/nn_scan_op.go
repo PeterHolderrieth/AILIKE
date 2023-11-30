e89da2836f4e86049fa1a522c689152c380df523
Tally Portnoi <tally38@gmail.com>
2023-11-30 15:29:48 -0500

    VectorIndex -> NNScan

--- go-db/godb/nn_scan_op.go
+++ go-db/godb/nn_scan_op.go
@@ -7,7 +7,7 @@ func vectorIndexExists(field FieldType, c *Catalog) bool {
 	return true
 }
 
-type VectorIndex struct {
+type NNScan struct {
 	indexField  FieldType
 	queryVector ConstExpr
 	heapFile    HeapFile // tempory hack to continue doing heap scans until vector index implemented
@@ -16,23 +16,23 @@ type VectorIndex struct {
 }
 
 // Create an
-func NewVectorIndex(heapFile HeapFile, limit Expr, indexField FieldType, queryVector ConstExpr, ascending bool) (*VectorIndex, error) {
+func NewNNScan(heapFile HeapFile, limit Expr, indexField FieldType, queryVector ConstExpr, ascending bool) (*NNScan, error) {
 	// TODO: add validation on inputs
-	return &VectorIndex{indexField, queryVector, heapFile, limit, ascending}, nil
+	return &NNScan{indexField, queryVector, heapFile, limit, ascending}, nil
 }
 
-func (v *VectorIndex) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
+func (v *NNScan) Iterator(tid TransactionID) (func() (*Tuple, error), error) {
 	return v.heapFile.Iterator(tid)
 }
 
-func (v *VectorIndex) Descriptor() *TupleDesc {
+func (v *NNScan) Descriptor() *TupleDesc {
 	return v.heapFile.Descriptor()
 }
 
-func (v *VectorIndex) PrettyPrint() string {
+func (v *NNScan) PrettyPrint() string {
 	limitVal, err := v.limitTups.EvalExpr(nil) // using nil since limitTups is a ConstExpr that does not depend on tuple
 	if err != nil {
-		panic("Cannot evaluate limit within VectorIndex PrettyPrint.")
+		panic("Cannot evaluate limit within NNScan PrettyPrint.")
 	}
 	limit := limitVal.(IntField).Value
 	query := v.queryVector.val.(EmbeddedStringField).Value
