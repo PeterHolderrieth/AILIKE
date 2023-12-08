ca416a599d5c7befb61598d808da4c5af59a5347
Peter Holderrieth <peterholderrieth@31-38-85.wireless.csail.mit.edu>
2023-12-08 17:39:56 -0500

    Deleted test.dat:

--- go-db/godb/heap_file_wiki_test.go
+++ go-db/godb/heap_file_wiki_test.go
@@ -32,22 +32,26 @@ func TestConstructWikiHeapFile(t *testing.T) {
 	}
 }
 
+const recreateWiki = false
+
 func TestConstructFullWikiHeapFile(t *testing.T) {
-	bp := NewBufferPool(100)
-	hfile, err := ConstructWikiHeapFile("wiki_full", bp, true, 6458665)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	fmt.Println("Number of tuples in heap file: ", hfile.ApproximateNumTuples())
-	tid := NewTID()
-	hfileIter, err := hfile.Iterator(tid)
-	if err != nil {
-		t.Fatalf(err.Error())
-	}
-	for tuple, err := hfileIter(); (tuple != nil) && (err == nil); tuple, err = hfileIter() {
-		fmt.Println(tuple.PrettyPrintString(true))
-	}
-	if err != nil {
-		t.Fatalf(err.Error())
+	if recreateWiki {
+		bp := NewBufferPool(100)
+		hfile, err := ConstructWikiHeapFile("wiki_full", bp, true, 6458665)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		fmt.Println("Number of tuples in heap file: ", hfile.ApproximateNumTuples())
+		tid := NewTID()
+		hfileIter, err := hfile.Iterator(tid)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		for tuple, err := hfileIter(); (tuple != nil) && (err == nil); tuple, err = hfileIter() {
+			fmt.Println(tuple.PrettyPrintString(true))
+		}
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
 	}
 }
