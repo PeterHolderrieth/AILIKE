4d9dc746b377094a11f52789789fd2e872b6e4c5
Manya-bansal <manya227@stanford.edu>
2023-12-12 10:29:18 -0500

    cp

--- go-db/godb/benchmark_test.go
+++ go-db/godb/benchmark_test.go
@@ -26,7 +26,7 @@ func TestBenchmarkingInfra(t *testing.T) {
 // Benchmarking Functions
 type fn (func(string) string)
 
-func varyTableOnly(tables []string, catalog string, path string, query_gen []fn, dataDir string) {
+func varyTableOnly(tables []string, catalog string, path string, query_gen []fn, dataDir string, save bool) {
 
 	timing_csv_path := dataDir + "/times.csv"
 	timing_csv, err := os.OpenFile(timing_csv_path, os.O_RDWR|os.O_CREATE, 0644)
@@ -39,7 +39,7 @@ func varyTableOnly(tables []string, catalog string, path string, query_gen []fn,
 		time := int64(0)
 		for _, queries := range query_gen {
 			var config = newBenchMetaData(catalog, path, 1000,
-				dataDir, false, warmup_iter)
+				dataDir, save, warmup_iter)
 			fmt.Println("Starting ", table, " with ", queries(table))
 			time_taken, err := BenchmarkingInfra(table, queries(table), config)
 			if err != nil {
@@ -75,16 +75,25 @@ func TestVaryDBSize(t *testing.T) {
 		"tweets", "tweets_2500",
 		"tweets_5000", "tweets_10000", "tweets_20000",
 		"tweets_2500_c_5", "tweets_5000_c_10", "tweets_10000_c_20",
-		"tweets_20000_c_40",
+		"tweets_20000_c_40", "tweets_c_80",
 	}
+	// varyTableOnly(tables, catalog, catalog_path,
+	// 	[]fn{query_gen_agg("the migration patterns of professor hair"),
+	// 		query_gen("the migration patterns of professor hair", 5),
+	// 		query_gen_agg("I am so happy"),
+	// 		query_gen("I am so happy", 5),
+	// 		query_gen_agg("grad school is amazing"),
+	// 		query_gen("grad school is amazing", 5)},
+	// 	"./benchmark_results/var_db_size", false)
+
 	varyTableOnly(tables, catalog, catalog_path,
-		[]fn{query_gen_agg("the migration patterns of professor hair"),
+		[]fn{query_gen("the migration patterns of professor hair", 1),
 			query_gen("the migration patterns of professor hair", 5),
-			query_gen_agg("I am so happy"),
-			query_gen("I am so happy", 5),
-			query_gen_agg("grad school is amazing"),
+			query_gen("I am so happy", 1), 
+			query_gen("I am so happy", 5), 
+			query_gen("grad school is amazing", 1), 
 			query_gen("grad school is amazing", 5)},
-		"./benchmark_results/var_db_size")
+		"./benchmark_results/var_db_size", false)
 }
 
 func TestVaryNewQuery(t *testing.T) {
@@ -100,7 +109,7 @@ func TestVaryNewQuery(t *testing.T) {
 			query_gen_agg("I am so happy"),
 			query_gen_agg("I want to do to the doctor"),
 			query_gen_agg("I am a grad student")},
-		"./benchmark_results/var_agg_query")
+		"./benchmark_results/var_agg_query", false)
 }
 
 func TestVaryNumCluster(t *testing.T) {
@@ -118,13 +127,13 @@ func TestVaryNumCluster(t *testing.T) {
 			query_gen("I am so happy", 5), 
 			query_gen("grad school is amazing", 1), 
 			query_gen("grad school is amazing", 5)},
-		"./benchmark_results/var_num_cluster")
+		"./benchmark_results/var_num_cluster", false)
 }
 
 
 type fn_n (func(string, int) string)
 
-func varyTableAndN(tables []string, N []int, catalog string, path string, query_gen []fn_n, dataDir string) {
+func varyTableAndN(tables []string, N []int, catalog string, path string, query_gen []fn_n, dataDir string, save bool) {
 
 	timing_csv_path := dataDir + "/times.csv"
 	timing_csv, err := os.OpenFile(timing_csv_path, os.O_RDWR|os.O_CREATE, 0644)
@@ -139,7 +148,7 @@ func varyTableAndN(tables []string, N []int, catalog string, path string, query_
 		for _, n := range N {
 			for i, queries := range query_gen {
 				var config = newBenchMetaData(catalog, path, 1000,
-					dataDir, true, warmup_iter)
+					dataDir, save, warmup_iter)
 				DefaultProbe = n/3 + 1;
 				time_taken, err := BenchmarkingInfra(table+"_"+strconv.Itoa(n)+"_"+strconv.Itoa(i), queries(table, n), config)
 				if err != nil {
@@ -168,12 +177,12 @@ func TestVaryLimit(t *testing.T) {
 	tables := []string{"tweets", "tweets_c_80",
 		"clustered_tweets_c_80",
 	}
-	N := []int{1, 8, 64, 512, 4096}
+	N := []int{1, 8, 64, 512, 4096, 8000, 16000, 40000}
 	varyTableAndN(tables, N, catalog, catalog_path,
 		[]fn_n{query_gen_limit("the migration patterns of professor hair"),
 		query_gen_limit("I am so happy"),
 		query_gen_limit("grad school is amazing")},
-		"./benchmark_results/var_n")
+		"./benchmark_results/var_n", false)
 }
 
 func varyTableAndSetVar(tables []string, N []int, catalog string, path string, query_gen []fn, dataDir string, set_var func(int)) {
@@ -251,7 +260,7 @@ func varyBufferPoolAndVary(tables []string, N []int, catalog string, path string
 
 	for _, table := range tables {
 		time := int64(0)
-		for _, p := range P{
+		for _, p := range []int{0, 1}{
 		for _, n := range N {
 			for i, queries := range query_gen {
 				var config = newBenchMetaData(catalog, path, 1000,
@@ -263,15 +272,15 @@ func varyBufferPoolAndVary(tables []string, N []int, catalog string, path string
 				}
 				time_taken, err := BenchmarkingInfra(table+"_"+strconv.Itoa(p)+"_"+strconv.Itoa(n)+"_"+strconv.Itoa(i), queries(table, i), config)
 				if err != nil {
-					fmt.Println("Failed to run ", queries(table))
+					fmt.Println("Failed to run ", queries(table, i))
 					fmt.Println(err)
 				}
 				time += time_taken
 			}
 			fmt.Println("Time taken for ", table, " = ", time/int64(len(query_gen)))
-			fmt.Fprintf(timing_csv, "%s, %v\n", table+"_"+strconv.Itoa(p)+"_"+strconv.Itoa(n)+"_", time/int64(len(query_gen)))
+			fmt.Fprintf(timing_csv, "%s, %v\n", table+"_"+strconv.Itoa(p)+"_"+strconv.Itoa(n), time/int64(len(query_gen)))
 		}
-	}
+		}
 	}
 	timing_csv.Close()
 }
